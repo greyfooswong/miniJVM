0db8a5dd419f1c393a6a8f74cf91c1c3b77f2678
southli <961065551@qq.com>
2021-10-06 20:09:24 +0800

    my own jvm

---
+++ miniJVM/instructions/constants/nop.go
@@ -0,0 +1,11 @@
+package constants
+
+import "miniJVM/instructions/base"
+import "miniJVM/rtda"
+
+// Do nothing
+type NOP struct{ base.NoOperandsInstruction }
+
+func (self *NOP) Execute(frame *rtda.Frame) {
+	// really do nothing
+}
---
+++ miniJVM/instructions/control/goto.go
@@ -0,0 +1,11 @@
+package control
+
+import "miniJVM/instructions/base"
+import "miniJVM/rtda"
+
+// Branch always
+type GOTO struct{ base.BranchInstruction }
+
+func (self *GOTO) Execute(frame *rtda.Frame) {
+	base.Branch(frame, self.Offset)
+}
